9f579b1b0730b856efaeb3ad8174ab1824af0ae1
Clement Bellanger <bellanger.consultant@gmail.com>
2023-08-10 21:38:14 +0200

    Configutation mongoDB

---
+++ components/AddTask.tsx
@@ -0,0 +1,39 @@
+import { Flex, Heading, Text, Input, Button } from "@chakra-ui/react"
+import { SmallAddIcon } from "@chakra-ui/icons"
+
+
+import {AddTaskProps} from '@types'
+
+const AddTask = ({task, setTask, handleCreateTask}: AddTaskProps) => {
+  return (
+   
+<Flex pt="2rem" pl="2rem" pr ="2rem" pb ="1rem">
+
+    <Input placeholder='nouvelle tâche...'
+    size="1g"
+    onChange={(e)=> setTask(e.target.value)}
+    value = {task}
+    style={{'background': '#fff'}}/>
+    <Button 
+        colorScheme='twitter'
+        size='1g'
+        onClick={() => handleCreateTask()}
+
+> <SmallAddIcon />
+
+
+
+    </Button>
+
+
+</Flex>
+
+
+
+
+
+
+  )
+}
+
+export default AddTask
