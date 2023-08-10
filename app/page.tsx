ecea7f0de118721a85e3b1805c80005244f23b65
Clement Bellanger <bellanger.consultant@gmail.com>
2023-08-10 22:02:57 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,29 @@ import AddTask from "@components/AddTask"
 export default function Home() {
   
   const [task,setTask] = useState("")
+  const [isLoading, setIsLoading] = useState(true)
   
   const handleCreateTask = async() => {
+    setIsLoading(true)
+    try {
+      const response = await fetch("/api/task/new")
+      method :'POST'
+      body: JSON.stringify(
+       {
+        task:task
+       })
+       if (response.ok) {
+        setTask("")}
+        else{
+          console.log('error')
+        }
 
+
+
+    } catch (error) {
+      console.log(error)
+    }
+setIsLoading(false)
   }
   return (
     <>
